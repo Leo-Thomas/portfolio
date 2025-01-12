9bcd61cb64b9c2c4759f797cd1dc14dfdd0fc5bd
Leo-Thomas <91854676+Leo-Thomas@users.noreply.github.com>
2025-01-12 17:03:03 +0100

    Update PageTitle.js

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet-async";
 const PageTitle = ({ title }) => {
   return (
     <Helmet>
-      <title>{title} - Leo Thomas Ramos webpage</title>
+      <title>{title} - Leo Thomas Ramos </title>
     </Helmet>
   );
 };
